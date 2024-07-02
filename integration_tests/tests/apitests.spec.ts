23afcef431b1486c643fdf4df0b5d301a745cfbe
ilyayaroshevich <ilya.yaroshevich@internet.ru>
2024-07-02 23:20:56 +0300

    fixes

--- integration_tests/tests/apitests.spec.ts
+++ integration_tests/tests/apitests.spec.ts
@@ -1,11 +1,11 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals'
 import {
   getRequest,
   postRequest,
   putRequest,
   patchRequest,
   deleteRequest,
-} from '../consts/methods_functions';
+} from '../consts/methods_functions'
 import {
   bodyPost,
   bodyComments,
@@ -23,214 +23,217 @@ import {
   bodytWithBodytoPatch,
   emptyBodytoPatch,
   randomPost,
-} from '../consts/consts';
+} from '../consts/consts'
 
 describe('GET TESTS', () => {
-  describe('GetApiTests. POSITIVE', () => {
+  describe('POSITIVE', () => {
     test('./posts', async () => {
-      const response = await getRequest('https://jsonplaceholder.typicode.com/posts');
-      expect(response.status).toBe(200);
-    });
-    test('GET./posts/{post_number}', async () => {
-      const response = await getRequest(`https://jsonplaceholder.typicode.com/posts/${randomPost}`);
+      const response = await getRequest('https://jsonplaceholder.typicode.com/posts')
+      expect(response.status).toBe(200)
+    })
+    test('./posts/{post_number}', async () => {
+      const response = await getRequest(`https://jsonplaceholder.typicode.com/posts/${randomPost}`)
       expect(response).toMatchObject({
         status: 200,
         body: bodyPost,
-      });
-    });
+      })
+    })
     test('./posts/{post_id}/comments', async () => {
       const response = await getRequest(
         `https://jsonplaceholder.typicode.com/posts/${randomPost}/comments`,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body).toHaveLength(5);
-    });
-    test('./comments?postId=1', async () => {
+      )
+      expect(response.status).toBe(200)
+      expect(response.body).toHaveLength(5)
+    })
+    test('./comments?postId={post_id}', async () => {
       const response = await getRequest(
         `https://jsonplaceholder.typicode.com/comments?postId=${randomPost}`,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body[0]).toMatchObject(bodyComments);
-    });
-  });
-  describe('GetApiTests. NEGATIVE', () => {
+      )
+      expect(response.status).toBe(200)
+      expect(response.body[0]).toMatchObject(bodyComments)
+    })
+  })
+  describe('NEGATIVE', () => {
     test('./posts/{post_number}.Non-existent post', async () => {
-      const response = await getRequest('https://jsonplaceholder.typicode.com/posts/1000');
-      expect(response.status).toBe(404);
-      expect(response.body).toBeUndefined();
-    });
-  });
-});
+      const response = await getRequest('https://jsonplaceholder.typicode.com/posts/1000')
+      expect(response.status).toBe(404)
+      expect(response.body).toBeUndefined()
+    })
+  })
+})
 
 describe('POST TESTS', () => {
-  describe('PostApiTests. POSITIVE', () => {
+  describe('POSITIVE', () => {
     test('Creating post full valid. /posts', async () => {
       const response = await postRequest(
         `https://jsonplaceholder.typicode.com/posts`,
         fullValidBodyToPost,
-      );
-      expect(response.status).toBe(201);
-      expect(response.body.title).toEqual(fullValidBodyToPost.title);
-      expect(response.body.body).toEqual(fullValidBodyToPost.body);
-      expect(response.body.userId).toEqual(fullValidBodyToPost.userId);
-    });
+      )
+      expect(response.status).toBe(201)
+      expect(response.body.title).toEqual(fullValidBodyToPost.title)
+      expect(response.body.body).toEqual(fullValidBodyToPost.body)
+      expect(response.body.userId).toEqual(fullValidBodyToPost.userId)
+    })
     test('Creating without title. /posts', async () => {
       const response = await postRequest(
         `https://jsonplaceholder.typicode.com/posts`,
         ValidBodyWithoutTitle,
-      );
-      expect(response.status).toBe(201);
-      expect(response.body.body).toEqual(ValidBodyWithoutTitle.body);
-      expect(response.body.userId).toEqual(ValidBodyWithoutTitle.userId);
-    });
+      )
+      expect(response.status).toBe(201)
+      expect(response.body.body).toEqual(ValidBodyWithoutTitle.body)
+      expect(response.body.userId).toEqual(ValidBodyWithoutTitle.userId)
+    })
     test('Creating without body. /posts', async () => {
       const response = await postRequest(
         `https://jsonplaceholder.typicode.com/posts`,
         ValidBodyWithoutBody,
-      );
-      expect(response.status).toBe(201);
-      expect(response.body.title).toEqual(ValidBodyWithoutBody.title);
-      expect(response.body.userId).toEqual(ValidBodyWithoutBody.userId);
-    });
+      )
+      expect(response.status).toBe(201)
+      expect(response.body.title).toEqual(ValidBodyWithoutBody.title)
+      expect(response.body.userId).toEqual(ValidBodyWithoutBody.userId)
+    })
     test('Creating without UserId. /posts', async () => {
       const response = await postRequest(
         `https://jsonplaceholder.typicode.com/posts`,
         ValidBodyWithoutUserId,
-      );
-      expect(response.status).toBe(201);
-      expect(response.body.body).toEqual(ValidBodyWithoutUserId.body);
-      expect(response.body.title).toEqual(ValidBodyWithoutUserId.title);
-      expect(typeof response.body.body).toBe('string');
-      expect(typeof response.body.title).toBe('string');
-    });
-    describe('PostApiTests. NEGATIVE', () => {
-      test('Creating post with incorrect types. /posts', async () => {
-        const response = await postRequest(
-          `https://jsonplaceholder.typicode.com/posts`,
-          fullInvalidBodyToPost,
-        );
-        expect(response.status).toBe(201);
-      });
-    });
-  });
-});
+      )
+      expect(response.status).toBe(201)
+      expect(response.body.body).toEqual(ValidBodyWithoutUserId.body)
+      expect(response.body.title).toEqual(ValidBodyWithoutUserId.title)
+      expect(typeof response.body.body).toBe('string')
+      expect(typeof response.body.title).toBe('string')
+    })
+  })
+  describe('NEGATIVE', () => {
+    test('Creating post with incorrect types. /posts', async () => {
+      const response = await postRequest(
+        `https://jsonplaceholder.typicode.com/posts`,
+        fullInvalidBodyToPost,
+      )
+      expect(response.status).toBe(201)
+    })
+  })
+})
 
 describe('PUT TESTS', () => {
-  describe('PutApiTests. POSITIVE', () => {
+  describe('POSITIVE', () => {
     test('Min boundery in values', async () => {
       const response = await putRequest(
         `https://jsonplaceholder.typicode.com/posts/55`,
         fullValidBodyToPut,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body.id).toEqual(fullValidBodyToPut.id);
-      expect(response.body.userId).toEqual(fullValidBodyToPut.userId);
-      expect(response.body.body).toEqual(fullValidBodyToPut.body);
-      expect(response.body.title).toEqual(fullValidBodyToPut.title);
-    });
+      )
+      expect(response.status).toBe(200)
+      expect(response.body.id).toEqual(fullValidBodyToPut.id)
+      expect(response.body.userId).toEqual(fullValidBodyToPut.userId)
+      expect(response.body.body).toEqual(fullValidBodyToPut.body)
+      expect(response.body.title).toEqual(fullValidBodyToPut.title)
+    })
     test('Big data in values', async () => {
       const response = await putRequest(
         `https://jsonplaceholder.typicode.com/posts/100`,
         fullValidBodyToPutBigData,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body.id).toEqual(fullValidBodyToPutBigData.id);
-      expect(response.body.userId).toEqual(fullValidBodyToPutBigData.userId);
-      expect(response.body.body).toEqual(fullValidBodyToPutBigData.body);
-      expect(response.body.title).toEqual(fullValidBodyToPutBigData.title);
-    });
+      )
+      expect(response.status).toBe(200)
+      expect(response.body.id).toEqual(fullValidBodyToPutBigData.id)
+      expect(response.body.userId).toEqual(fullValidBodyToPutBigData.userId)
+      expect(response.body.body).toEqual(fullValidBodyToPutBigData.body)
+      expect(response.body.title).toEqual(fullValidBodyToPutBigData.title)
+    })
     test('Null in values', async () => {
       const response = await putRequest(
         `https://jsonplaceholder.typicode.com/posts/1`,
         differentTypesForBodyToPut,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body.id).toEqual(differentTypesForBodyToPut.id);
-      expect(response.body.userId).toBeNull();
-      expect(response.body.body).toBeNull();
-      expect(response.body.title).toBeNull();
-    });
-  });
-  describe('PutApiTests. NEGATIVE', () => {
+      )
+      expect(response.status).toBe(200)
+      expect(response.body.id).toEqual(differentTypesForBodyToPut.id)
+      expect(response.body.userId).toBeNull()
+      expect(response.body.body).toBeNull()
+      expect(response.body.title).toBeNull()
+    })
+  })
+  describe('NEGATIVE', () => {
     test('Non-existed post', async () => {
       const response = await putRequest(
         `https://jsonplaceholder.typicode.com/posts/101`,
         fullValidBodyToPut,
-      );
-      expect(response.status).toBe(500);
-    });
+      )
+      expect(response.status).toBe(500)
+    })
     test('Without posts', async () => {
       const response = await putRequest(
         `https://jsonplaceholder.typicode.com/posts/`,
         fullValidBodyToPut,
-      );
-      expect(response.status).toBe(404);
-    });
-  });
-});
+      )
+      expect(response.status).toBe(404)
+    })
+  })
+})
 
 describe('PATCH TESTS', () => {
-  describe('PatchApiTests. POSITIVE', () => {
+  describe('POSITIVE', () => {
     test('Only id', async () => {
       const response = await patchRequest(
         `https://jsonplaceholder.typicode.com/posts/${randomPost}`,
         bodytWithIdtoPatch,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body.id).toEqual(bodytWithIdtoPatch.id);
-    });
+      )
+      expect(response.status).toBe(200)
+      expect(response.body.id).toEqual(bodytWithIdtoPatch.id)
+    })
     test('Only userId', async () => {
       const response = await patchRequest(
         `https://jsonplaceholder.typicode.com/posts/${randomPost}`,
         bodytWithUserIdtoPatch,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body.userId).toEqual(bodytWithUserIdtoPatch.userId);
-    });
+      )
+      expect(response.status).toBe(200)
+      expect(response.body.userId).toEqual(bodytWithUserIdtoPatch.userId)
+    })
     test('Only title', async () => {
       const response = await patchRequest(
         `https://jsonplaceholder.typicode.com/posts/${randomPost}`,
         bodytWithTitletoPatch,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body.title).toEqual(bodytWithTitletoPatch.title);
-    });
+      )
+      expect(response.status).toBe(200)
+      expect(response.body.title).toEqual(bodytWithTitletoPatch.title)
+    })
     test('Only body', async () => {
       const response = await patchRequest(
         `https://jsonplaceholder.typicode.com/posts/55`,
         bodytWithBodytoPatch,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body.body).toEqual(bodytWithBodytoPatch.body);
-    });
+      )
+      expect(response.status).toBe(200)
+      expect(response.body.body).toEqual(bodytWithBodytoPatch.body)
+    })
     test('Non-existed post', async () => {
       const response = await patchRequest(
         `https://jsonplaceholder.typicode.com/posts/10000`,
         emptyBodytoPatch,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body).toBeDefined();
-      expect(response.title).toBeUndefined();
-    });
-    test('Text in request', async () => {
+      )
+      expect(response.status).toBe(200)
+      expect(response.body).toBeDefined()
+      expect(response.title).toBeUndefined()
+    })
+  })
+  describe('NEAGTIVE', () => {
+    test('Incorrect request', async () => {
       const response = await patchRequest(
-        `https://jsonplaceholder.typicode.com/posts/test`,
+        `https://jsonplaceholder.typicode.com/postss/test`,
         emptyBodytoPatch,
-      );
-      expect(response.status).toBe(200);
-      expect(response.body).toBeDefined();
-      expect(response.title).toBeUndefined();
-    });
-  });
-});
-
+      )
+      expect(response.status).toBe(404)
+    })
+  })
+})
 describe('DELETE TESTS', () => {
-  test('Delete existed post', async () => {
-    const response = await deleteRequest(`https://reqres.in/api/users/${randomPost}`);
-    expect(response.status).toBe(204);
-  });
-  test('Incorrect request', async () => {
-    const response = await deleteRequest(`https://reqres.in/apsi/users/100`);
-    expect(response.status).toBe(404);
-  });
-});
+  describe('POSITIVE', () => {
+    test('Delete existed post', async () => {
+      const response = await deleteRequest(`https://reqres.in/api/users/${randomPost}`)
+      expect(response.status).toBe(204)
+    })
+  })
+  describe('NEGATIVE', () => {
+    test('Incorrect request', async () => {
+      const response = await deleteRequest(`https://reqres.in/apsi/users/100`)
+      expect(response.status).toBe(404)
+    })
+  })
+})
